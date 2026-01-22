84f79034570cac41a4f1312c45d313816cd0809a
iamVihanga <vihangarashansilva@gmail.com>
2026-01-22 09:08:10 +0530

    feat: bitwise operators

--- Operators/Main.java
+++ Operators/Main.java
@@ -22,5 +22,44 @@ public class Main {
             System.out.println("Lights off");
         }
 
+        scanner.close();
+
+        // Bitwise Operators
+        int bitwiseAnd = 5 & 3;
+        System.out.println(bitwiseAnd);
+
+        int bitwiseOr = 5 | 3;
+        System.out.println(bitwiseOr);
+
+        int bitwiseXor = 5 ^ 3;
+        System.out.println(bitwiseXor);
+
+        int leftShift = 5 << 2;
+        System.out.println(leftShift);
+        /*
+         * Explaination for left shift
+         * 5 = 101
+         * 2 = 10
+         * 101 << 10 = 10100
+         */
+
+        int rightShift = 5 >> 2;
+        System.out.println(rightShift);
+        /*
+         * Explaination for right shift
+         * 5 = 101
+         * 2 = 10
+         * 101 >> 10 = 1
+         */
+
+        int unsignedRightShift = 5 >>> 2;
+        System.out.println(unsignedRightShift);
+        /*
+         * Explaination for unsigned right shift
+         * 5 = 101
+         * 2 = 10
+         * 101 >>> 10 = 1
+         */
+
     }
 }
